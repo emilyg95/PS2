2d1498282192372418d4118ea5511e8df9c82196
Emily Garner <emilyg1995@yahoo.com>
2018-02-05 16:16:20 -0600

    cut
    removed earlier experimental attempt

--- PS2.R
+++ PS2.R
@@ -3,7 +3,7 @@ Benford = function(x, control){
   freq = table(digit)
   m = freq/sum(freq)
   for (i in 1:9){
-    Leemis = m - log10(1+1/i)
+    Leemis = max(m - log10(1+1/i))
     ChoVec = (m - log10(1+1/i))^2
     Cho_Gains = sqrt(sum(ChoVec))
   }
@@ -22,24 +22,23 @@ Benford = function(x, control){
 
 Benford(1:9, 1)
 
+
 print.benfords = function(x, control){
   digit = substr(x, 1, 1)
   freq = table(digit)
   m = freq/sum(freq)
   for (i in 1:9){
-    Leemis = as.data.frame(m - log10(1+1/i), responseName = "Leemis", stringsAsFactors = F)
+    Leemis = max(m - log10(1+1/i))
     ChoVec = (m - log10(1+1/i))^2
-    Cho_Gains = as.data.frame(sqrt(sum(ChoVec)), responseName = "Cho_Gains")
-    colnames(Cho_Gains) = c("Cho_Gains")
-    names(Leemis) = c("Last_Digits", "Leemis")
-    Stars_L = symnum(Leemis, cutpoints = c(0, 0.851,  0.967, 1.212, 100),
-                    symbols = c("*","**","***"," "))
-    Signif_L = cbind(Leemis, Stars_L)
-    Stars_C = symnum(Cho_Gains, cutpoints = c(0, 1.212,  1.330, 1.212, 1.569),
-                     symbols = c("*","**","***"," "))
-    Signif_C = cbind(Cho_Gains, Stars_C)
+    Cho_Gains = sqrt(sum(ChoVec))
   }
-
+  Stars_L = symnum(Leemis, cutpoints = c(0, 0.851,  0.967, 1.212, 100), corr = F,
+                   symbols = c("cannot reject null","*","**","***"))
+  Signif_L = cbind("Leemis", Leemis, Stars_L)
+  Stars_C = symnum(Cho_Gains, cutpoints = c(0, 1.212,  1.330, 1.569, 100), corr = F,
+                   symbols = c("cannot reject null","*","**","***"))
+  Signif_C = cbind("Cho Gains", Cho_Gains, Stars_C)
+  
   if (control == 1){
     return(Signif_L)
   }
@@ -51,53 +50,57 @@ print.benfords = function(x, control){
     return(c(Signif_L,Signif_C))
   }
 }
-
 print.benfords(1:9, 3)
 
 
 
 
-symp <- symnum(Leemis, corr = FALSE,
-               cutpoints = c(0.851,  0.967, 1.212),
-               symbols = c("***","**","*","."," "))
 
-  print.benfords(4:9, 3)
-x = 1:9
-
-
-
-
-
-
-
-
-
-
-names(print.benfords(x,2))
 
 print.benfords = function(x, control){
   digit = substr(x, 1, 1)
   freq = table(digit)
   m = freq/sum(freq)
   for (i in 1:9){
-    Leemis = as.matrix(m - log10(1+1/i), responseName = "Leemis", stringsAsFactors = F)
+    Leemis = as.data.frame(max(m - log10(1+1/i)))
     ChoVec = (m - log10(1+1/i))^2
     Cho_Gains = as.data.frame(sqrt(sum(ChoVec)))
-  }
-  
-  names(Leemis) = c("Digit", "Leemis")
-  
+    colnames(Cho_Gains) = c("Cho_Gains")
+    colnames(Leemis) = c("Leemis")
+    if (max(Leemis) <= 0.851){
+      print.noquote("Leemis: cannot reject the null")
+    }else if (0.967 >= max(Leemis) & max(Leemis) > 0.851){
+      print.noquote("Leemis: *")
+    }else if (1.212 >= max(Leemis) & max(Leemis) > 0.967){
+      print.noquote("Leemis: **")
+    }else{
+      print.noquote("Leemis: ***")
+    }
+    if ((Cho_Gains) <= 1.212){
+      print.noquote("Cho Gains: cannot reject the null")
+    }else if (1.330 >= (Cho_Gains) & (Cho_Gains) > 1.212){
+      print.noquote("Cho Gains: *")
+    }else if (1.569 >= (Cho_Gains) & (Cho_Gains) > 1.330){
+      print.noquote("Cho Gains: **")
+    }else{
+      print.noquote("Cho Gains: ***")
+    }
   if (control == 1){
     return(Leemis)
-    colnames(print.benfords) = c("First Digit", "Leemis")
-  }
-  
-  if (control == 2){
+  }else if (control == 2){
     return(Cho_Gains)
-    names(print.benfords) = c("Cho Gains")
-  }
-  else{
-    return(c(Leemis,Cho_Gains))
-    colnames(print.benfords) = c("First Digit", "Leemis", "Cho Gains")
+  }else{
+    print.noquote("Significance Levels; a = 0.1*, a = 0.05**, a = 0.01***")
+    return(c(Leemis, Cho_Gains))
   }
 }
+}
+print.benfords(1:9, 3)$Leemis
+getwd()
+setwd("/Users/emilygarner/Documents/School/Second Sem/R/Problem Sets/PS2")
+sink.benfords = function(x){
+  sink(file = "Print_Benfords.csv")
+  print.benfords(1:9, 3)$Leemis
+  sink()
+}
+sink.benfords(1:9)
